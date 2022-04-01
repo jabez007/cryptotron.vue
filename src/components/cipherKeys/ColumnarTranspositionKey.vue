6968838dce5945ab42480c438e823f6b2b2a1a3c
Jimmy <jabez007@users.noreply.github.com>
2022-04-01 15:14:47 -0500

    update Columnar Transposition

--- src/components/cipherKeys/ColumnarTranspositionKey.vue
+++ src/components/cipherKeys/ColumnarTranspositionKey.vue
@@ -1,23 +1,25 @@
 <template>
-  <v-form ref="form">
-    <v-layout row>
-      <v-text-field
-        label="Key Word"
-        v-model.trim="key.keyword"
-        :rules="rules"
-        required
-        clearable
-      ></v-text-field>
-    </v-layout>
-    <v-layout row>
-      <v-text-field
-        label="Sorted"
-        :value="(key.keyword || '').toLowerCase().split('').sort().join('')"
-        disabled
-        readonly
-      ></v-text-field>
-    </v-layout>
-  </v-form>
+  <vs-card class="card key">
+    <vs-row vs-justify="center">
+      <vs-col :vs-w="fullWidth ? 12 : 3">
+        <vs-input
+          description-text="Key Word"
+          v-model.trim="key.keyword"
+          required
+        ></vs-input>
+      </vs-col>
+    </vs-row>
+    <vs-row vs-justify="center">
+      <vs-col :vs-w="fullWidth ? 12 : 3">
+        <vs-input
+          description-text="Sorted"
+          :value="(key.keyword || '').toLowerCase().split('').sort().join('')"
+          disabled
+          readonly
+        ></vs-input>
+      </vs-col>
+    </vs-row>
+  </vs-card>
 </template>
 
 <script>
@@ -26,6 +28,12 @@ import mixin from './cipherKeysMixin';
 
 export default {
   mixins: [mixin],
+  props: {
+    fullWidth: {
+      type: Boolean,
+      default: false,
+    },
+  },
   computed: {
     rules() {
       return [Rules.required, Rules.word, Rules.minLength(2)];
--- src/components/cipherKeys/RailFenceKey.vue
+++ src/components/cipherKeys/RailFenceKey.vue
@@ -1,15 +1,17 @@
 <template>
-  <vs-row class="card key" vs-justify="center">
-    <vs-col :vs-w="fullWidth ? 12 : 2">
-      <vs-input-number
-        label="Number of Rails"
-        v-model.number="key.rails"
-        min="1"
-        size="large"
-        required
-      ></vs-input-number>
-    </vs-col>
-  </vs-row>
+  <vs-card class="card key">
+    <vs-row vs-justify="center">
+      <vs-col :vs-w="fullWidth ? 12 : 2">
+        <vs-input-number
+          label="Number of Rails"
+          v-model.number="key.rails"
+          min="1"
+          size="large"
+          required
+        ></vs-input-number>
+      </vs-col>
+    </vs-row>
+  </vs-card>
 </template>
 
 <script>
