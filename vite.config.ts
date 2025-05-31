aaffcd21285bebf71e396c3a00ca95c9d7637d81
jabez007 <jabez007@users.noreply.github.com>
2025-05-31 07:54:30 -0500

    fix base path for production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { name } from './package.json';
+
+const basePath = process.env.NODE_ENV === 'production'
+  ? `/${name}/`
+  : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [
     vue(),
     vueDevTools(),
