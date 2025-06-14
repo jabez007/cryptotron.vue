be038462b87ebecbe3ff64dc5bd8b2e668cb0db8
jabez007 <jabez007@users.noreply.github.com>
2025-06-14 09:53:55 -0500

    working more on flow chart basics

--- src/views/BuilderView.vue
+++ src/views/BuilderView.vue
@@ -1,32 +1,133 @@
 <!-- Flowchart.vue -->
 <script setup lang="ts">
 import { ref } from 'vue'
-import { VueFlow } from '@vue-flow/core'
+import { MarkerType, useVueFlow, VueFlow } from '@vue-flow/core'
+import { Background } from '@vue-flow/background'
+import { Controls } from '@vue-flow/controls'
 
 const nodes = ref([
-  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+  { id: '1', label: 'Node 1', position: { x: 250, y: 5 } },
   { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
   { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
   { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
 ])
 
 const edges = ref([
-  { id: 'e1-2', source: '1', target: '2', animated: true },
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2',
+    updatable: true,
+    animated: true,
+    markerEnd: MarkerType.Arrow,
+  },
   { id: 'e1-3', source: '1', target: '3' },
 ])
+
+const {
+  addEdges,
+  onConnect,
+  onEdgeDoubleClick,
+  onEdgeUpdate,
+  onNodeClick,
+  removeEdges,
+  updateEdge,
+} = useVueFlow()
+
+onNodeClick(({ event, node }) => {
+  console.debug('On Node Click', event, node)
+})
+
+onConnect((connection) => {
+  console.debug('On Connect', connection)
+  addEdges({
+    ...connection,
+    updatable: true,
+    animated: true,
+    markerEnd: MarkerType.Arrow,
+  })
+})
+
+onEdgeDoubleClick(({ edge, event }) => {
+  console.debug('One Edge Double Click', event, edge)
+  removeEdges(edge)
+})
+
+onEdgeUpdate(({ connection, edge }) => {
+  console.debug('On Edge Update', connection, edge)
+  updateEdge(edge, connection)
+})
 </script>
 
 <template>
-  <VueFlow v-model:nodes="nodes" v-model:edges="edges"></VueFlow>
+  <VueFlow class="dark basic-flow" :nodes="nodes" :edges="edges" :connection-radius="53" :edge-updater-radius="23"
+    fit-view-on-init>
+    <Background />
+    <Controls position="top-left"></Controls>
+  </VueFlow>
 </template>
 
 <style>
 @import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+@import '@vue-flow/controls/dist/style.css';
+
+.basic-flow.dark {
+  background: var(--cryptotron-darker-bg);
+  color: var(--cryptotron-text-primary);
+}
+
+/* nodes */
+.basic-flow.dark .vue-flow__node {
+  background: var(--cryptotron-dark-bg);
+  color: var(--cryptotron-text-primary);
+}
+
+.basic-flow.dark .vue-flow__node.selected {
+  background: var(--cryptotron-card-bg);
+  border: 1px solid var(--cryptotron-border-glow);
+  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.3);
+}
+
+/* edges */
+.basic-flow.dark .vue_flow__edge-textbg {
+  fill: #292524;
+}
+
+.basic-flow.dark .vue-flow__edge-text {
+  fill: #fffffb;
+}
+
+/* controls */
+.basic-flow.dark .vue-flow__controls {
+  border: none;
+  border-radius: 12px;
+}
+
+.basic-flow.dark .vue-flow__controls .vue-flow__controls-button {
+  border: 1px solid var(--cryptotron-border-glow);
+  background: #333;
+  fill: var(--cryptotron-text-secondary);
+}
+
+.basic-flow.dark .vue-flow__controls .vue-flow__controls-button:first-child {
+  border-top-right-radius: 12px;
+  border-top-left-radius: 12px;
+}
+
+.basic-flow.dark .vue-flow__controls .vue-flow__controls-button:last-child {
+  border-bottom-right-radius: 12px;
+  border-bottom-left-radius: 12px;
+}
+
+.basic-flow.dark .vue-flow__controls .vue-flow__controls-button:hover {
+  background: #4d4d4d;
+}
 </style>
 
 <style scoped>
 .vue-flow {
-  min-height: 33vh;
+  height: 33vh;
   border: 1px solid var(--cryptotron-border-glow);
   border-radius: 12px;
 }
