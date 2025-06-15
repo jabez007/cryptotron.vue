70abc6510ff5c948294dff8031b13a1fbe07cd6d
jabez007 <jabez007@users.noreply.github.com>
2025-06-15 18:11:45 -0500

    simplifying vite config based on PR recommendation

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import { name } from './package.json';
-
-const basePath = process.env.NODE_ENV === 'production'
-  ? `/${name.split('/').slice(-1)[0]}/`
-  : '/';
-
-console.debug(`Base path set to: ${basePath}`)
+import { name } from './package.json'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: basePath,
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? `/${name.split('/').pop()}/` : '/'
+
+  console.debug(`Base path set to: ${base}`)
+
+  return {
+    base,
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
