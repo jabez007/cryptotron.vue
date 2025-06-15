ba8bf66ded2cf153d220ed31c282edd4c2f8b5be
jabez007 <jabez007@users.noreply.github.com>
2025-06-15 18:35:56 -0500

    fixing routes import to work with browser bundlers based on PR recommendation

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import type { App } from 'vue';
-import type { Router } from 'vue-router';
-import CryptoTronRoutes from './router/routes.ts'
+import type { App } from 'vue'
+import type { Router } from 'vue-router'
+import CryptoTronRoutes from './router/routes'
 
 interface PluginOptions {
-  router: Router,
+  router: Router
   parentRouteName?: string
 }
 
 export default {
   app: () => import('./App.vue'),
   install(_app: App, options: PluginOptions) {
-
     CryptoTronRoutes(options.parentRouteName).forEach((r) => {
-
       if (!options.router.hasRoute(r.name)) {
         console.debug(`Adding ${r.name} to router with path '${r.path}'`)
 
@@ -23,12 +21,9 @@ export default {
           console.debug(`Adding to root`)
           options.router.addRoute(r)
         }
-
       } else {
         console.warn(`Route ${r.name} already exists on router`)
       }
-
     })
-
-  }
+  },
 }
